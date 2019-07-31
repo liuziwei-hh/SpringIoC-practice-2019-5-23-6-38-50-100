360b2897f8c14145a67f49c8401c530905d890f7
LZW <1312299240@qq.com>
2019-07-31 10:51:59 +0800

    hello

--- src/main/java/com/example/SpringIoCpractice/Main.java
+++ src/main/java/com/example/SpringIoCpractice/Main.java
@@ -1,1 +1,18 @@
 //TODO
+package com.example.SpringIoCpractice;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+import org.springframework.context.support.FileSystemXmlApplicationContext;
+import org.springframework.stereotype.Component;
+
+//TODO
+@Component
+public class Main{
+
+    public void testMain(){
+        ApplicationContext applicationContext = new FileSystemXmlApplicationContext("hello.xml");
+        HelloWorld helloWorld = (HelloWorld) applicationContext.getBean("HelloWorldBean");
+        helloWorld.getHello();
+    }
+}
--- src/test/java/com/example/SpringIoCpractice/SpringIoCPracticeApplicationTests.java
+++ src/test/java/com/example/SpringIoCpractice/SpringIoCPracticeApplicationTests.java
@@ -4,13 +4,16 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.beans.factory.annotation.Autowired;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class SpringIoCPracticeApplicationTests {
 
+	@Autowired
+	Main main;
 	@Test
 	public void contextLoads() {
+		main.testMain();
 	}
-
 }
